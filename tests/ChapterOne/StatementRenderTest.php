32def73a9c57fb5cf063342c6bf03633066a7ec1
Gleuton <gleuton.dutra@gmail.com>
2021-03-16 22:26:43 -0300

    refatoring test add setUp

--- tests/ChapterOne/StatementRenderTest.php
+++ tests/ChapterOne/StatementRenderTest.php
@@ -7,7 +7,10 @@ use PHPUnit\Framework\TestCase;
 
 class StatementRenderTest extends TestCase
 {
-    private string $result = 'Statement for BigCo
+    private object $invoices;
+    private object $plays;
+
+    private string $resultPlainText = 'Statement for BigCo
 Hamlet: $650.00 (55 seats)
 As You Like It: $580.00 (35 seats)
 Othello: $500.00 (40 seats)
@@ -15,7 +18,7 @@ Amount owed is $1,730.00
 You earner 47 credits
 ';
 
-    public function testStatementRenderReturnsPlainText(): void
+    protected function setUp(): void
     {
         $invoices_file = file_get_contents(
             __DIR__ . '/json/invoices.json'
@@ -23,26 +26,29 @@ You earner 47 credits
         $plays_file    = file_get_contents(
             __DIR__ . '/json/plays.json'
         );
-
         try {
-            $invoices = json_decode(
+            $this->invoices = json_decode(
                 $invoices_file,
                 false,
                 512,
                 JSON_THROW_ON_ERROR
             );
 
-            $plays = json_decode(
+            $this->plays = json_decode(
                 $plays_file,
                 false,
                 512,
                 JSON_THROW_ON_ERROR
             );
-
-            $text = (new StatementRender($invoices, $plays))->renderPlainText();
-            self::assertEquals($text, $this->result);
         } catch (\JsonException $e) {
             echo $e->getMessage();
         }
+    }
+
+    public function testStatementRenderReturnsPlainText(): void
+    {
+        $text = (new StatementRender($this->invoices, $this->plays))
+            ->renderPlainText();
+        self::assertEquals($text, $this->resultPlainText);
     }
 }
