4114bcefae7fa6b2eb5016c976ee2d215920a2a3
Gleuton <gleuton.dutra@gmail.com>
2021-03-18 12:15:31 -0300

    html render

--- src/ChapterOne/StatementRender.php
+++ src/ChapterOne/StatementRender.php
@@ -29,6 +29,26 @@ class StatementRender
         return $result . "You earner {$this->statementData->totalVolumeCredits} credits\n";
     }
 
+    public function renderHtml(): string
+    {
+        $totalAmount = $this->statementData->totalAmount;
+
+        $result = "<h1>Statement for {$this->statementData->customer}</h1>";
+        $result .= "<table>";
+        $result .= "<tr><th>Play</th><th>Seats</th><th>Cost</th></tr>";
+        foreach ($this->statementData->performances as $perf) {
+            $result .='<tr>';
+            $result .="<td>{$perf->play->name}</td>";
+            $result .="<td>{$perf->audience}</td>";
+            $result .="<td>{$this->usd($perf->amount)}</td>";
+            $result .='</tr>';
+        }
+        $result .= "</table>";
+        $result .= "<p>Amount owed is {$this->usd($totalAmount)}</p>";
+
+        return $result . "<p>You earner {$this->statementData->totalVolumeCredits} credits</p>";
+    }
+
     private function usd(float $value): string
     {
         return '$' . number_format($value, 2);
--- tests/ChapterOne/StatementRenderTest.php
+++ tests/ChapterOne/StatementRenderTest.php
@@ -8,13 +8,22 @@ use Refactoring\ChapterOne\StatementRender;
 class StatementRenderTest extends TestCase
 {
     private StatementLoadFiles $files;
-    private string $resultPlainText = 'Statement for BigCo
-Hamlet: $650.00 (55 seats)
-As You Like It: $580.00 (35 seats)
-Othello: $500.00 (40 seats)
-Amount owed is $1,730.00
-You earner 47 credits
-';
+    private string $resultPlainText = "Statement for BigCo\n" .
+    "Hamlet: $650.00 (55 seats)\n" .
+    "As You Like It: $580.00 (35 seats)\n" .
+    "Othello: $500.00 (40 seats)\n" .
+    "Amount owed is $1,730.00\n" .
+    "You earner 47 credits\n";
+
+    private string $resultHtml = '<h1>Statement for BigCo</h1>' .
+    '<table>' .
+    '<tr><th>Play</th><th>Seats</th><th>Cost</th></tr>' .
+    '<tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>' .
+    '<tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>' .
+    '<tr><td>Othello</td><td>40</td><td>$500.00</td></tr>' .
+    '</table>' .
+    '<p>Amount owed is $1,730.00</p>' .
+    '<p>You earner 47 credits</p>';
 
     protected function setUp(): void
     {
@@ -23,11 +32,21 @@ You earner 47 credits
 
     public function testStatementRenderReturnsPlainText(): void
     {
-        $text =(new StatementRender(
+        $text = (new StatementRender(
             $this->files->invoices,
             $this->files->plays
         ))->renderPlainText();
 
         self::assertEquals($this->resultPlainText, $text);
+    }
+
+    public function testStatementRenderReturnsHtml(): void
+    {
+        $html = (new StatementRender(
+            $this->files->invoices,
+            $this->files->plays
+        ))->renderHtml();
+
+        self::assertEquals($this->resultHtml, $html);
     }
 }
